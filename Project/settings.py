4ea6d6f28762459c3c0b995a4e83189809f8d2ec
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-03-03 13:18:31 +0530

    second commit

--- Project/settings.py
+++ Project/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
+from django.contrib.messages import constants as messages
 
 from pathlib import Path
 
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
